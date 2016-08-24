40bd4330da569f086f2510bcdc8c8d6497d5c3b0
xuqingfeng <js-xqf@hotmail.com>
2016-08-24 10:17:37 +0800

    feat: binary name

--- release.sh
+++ release.sh
@@ -6,13 +6,17 @@ shopt -s nullglob
 
 files=(*_*)
 
-#
 mkdir -p dist
 
-# TODO: 16/8/23
+binaryname=""
 binary_name(){
 
-    return $1
+    if [[ $1 == *"windows"* ]]; then
+        binaryname=`echo $1 | cut -d'_' -f1`
+        binaryname="$binaryname.exe"
+    else
+        binaryname=`echo $1 | cut -d'_' -f1`
+    fi
 }
 
 filename=""
@@ -27,13 +31,19 @@ for f in ${files[*]}; do
     dir="${filename%.*}"
     #echo $dir
     mkdir -p ./dist/$dir
-    # TODO: binary_name
-    mv $f ./dist/$dir/$f;
+    binary_name $f
+    mv $f ./dist/$dir/$binaryname;
 
     # [[  ]]
     if [[ $dir == *"darwin"* ]] || [[ $dir == *"windows"* ]]; then
-        zip -r ./dist/$dir.zip ./dist/$dir
+        # exclude parent folder
+        cd ./dist
+        zip -q -r ./$dir.zip ./$dir
+        cd ..
     else
-        tar -czf ./dist/$dir.tar.gz ./dist/$dir
+        # exclude parent folder
+        tar -czf ./dist/$dir.tar.gz -C ./dist $dir
     fi
+
+    rm -rf ./dist/$dir
 done
