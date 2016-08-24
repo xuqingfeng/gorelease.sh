921daa6afb4460267a3934f3a33297d39bd30e55
xuqingfeng <js-xqf@hotmail.com>
2016-08-24 10:59:36 +0800

    feat: include README* & LICENSE

--- release.sh
+++ release.sh
@@ -4,13 +4,16 @@ set -e
 
 shopt -s nullglob
 
+# files in current directory with '_' in their names
 files=(*_*)
 
 mkdir -p dist
 
+# get binary name
 binaryname=""
 binary_name(){
 
+    # add '.exe' to windows binary
     if [[ $1 == *"windows"* ]]; then
         binaryname=`echo $1 | cut -d'_' -f1`
         binaryname="$binaryname.exe"
@@ -19,6 +22,7 @@ binary_name(){
     fi
 }
 
+# '_' -> '-'
 filename=""
 underscore_to_dash(){
 
@@ -29,10 +33,14 @@ for f in ${files[*]}; do
 
     underscore_to_dash $f
     dir="${filename%.*}"
-    #echo $dir
     mkdir -p ./dist/$dir
     binary_name $f
-    mv $f ./dist/$dir/$binaryname;
+    mv $f ./dist/$dir/$binaryname
+
+    # README & LICENSE
+    # ignore file not exist error
+    cp README* ./dist/$dir/ 2>/dev/null || :
+    cp LICENSE ./dist/$dir/ 2>/dev/null || :
 
     # [[  ]]
     if [[ $dir == *"darwin"* ]] || [[ $dir == *"windows"* ]]; then
